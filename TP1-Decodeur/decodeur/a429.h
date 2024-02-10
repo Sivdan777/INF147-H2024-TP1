1f81dcf6d96b5912c54ccb08fd6efb916e5c9131
Sivdan777 <31362154+Sivdan777@users.noreply.github.com>
2024-02-10 12:26:08 -0500

    Delete TP1-Decodeur directory

--- TP1-Decodeur/decodeur/a429.h
+++
@@ -1,9 +0,0 @@
-void afficher_entete_decodeur (void);
-void afficher_entete_test (void);
-void afficher_mot_a429 (unsigned int valeur);
-//void decoder_mot_a429 (unsigned int valeur, int* est_corrompu,
-//                       double* numero_mot, double* donnee_bnr,
-//                       int* donnee_bcd1, int* donnee_bcd2,
-//                       int* donnee_bcd3, int* donnee_bcd4);
-
-void decoder_mot_a429 (double valeur);
